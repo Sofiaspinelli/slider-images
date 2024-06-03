6de8205972843292ecc57ad8735de325e99e7155
Sofiaspinelli <sofiaspinelli17@gmail.com>
2024-06-03 11:32:18 -0300

    add styles and box-icons

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import "./style.css";
+import 'boxicons/css/boxicons.min.css';
+import 'boxicons';
 
 const arrayImagenesSlider = [
     { id: 1, ruta: 'img/gato-1.jpg', texto: 'Gatito 1'},
@@ -9,6 +11,20 @@ const arrayImagenesSlider = [
     { id: 6, ruta: 'img/gato-6.jpg', texto: 'Gatito 6'},
 ]
 window.addEventListener('DOMContentLoaded', function () {
+
     const divSlider = document.querySelector('#slider')
     console.log(divSlider);
+
+    arrayImagenesSlider.forEach(function(imagen) {
+
+        const divSliderSection = document.createElement('div')
+        const imgTag = document.createElement('img')
+        imgTag.src = imagen.ruta
+        imgTag.alt = imagen.texto
+        divSliderSection.classList.add('slider-section')
+        divSliderSection.appendChild(imgTag)
+        divSlider.appendChild(divSliderSection)
+    })
+
 })
+
