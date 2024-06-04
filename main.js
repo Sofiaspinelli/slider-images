9463f5aa800b0c488577d6837e5634944a6eaad2
Sofiaspinelli <sofiaspinelli17@gmail.com>
2024-06-04 14:02:46 -0300

    add btn events

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import './style.css';
 import 'boxicons/css/boxicons.min.css';
 import 'boxicons';
 
@@ -10,21 +10,76 @@ const arrayImagenesSlider = [
     { id: 5, ruta: 'img/gato-5.jpg', texto: 'Gatito 5'},
     { id: 6, ruta: 'img/gato-6.jpg', texto: 'Gatito 6'},
 ]
-window.addEventListener('DOMContentLoaded', function () {
 
+window.addEventListener('DOMContentLoaded', function() {
+    
     const divSlider = document.querySelector('#slider')
-    console.log(divSlider);
+    //console.log(divSlider)
 
     arrayImagenesSlider.forEach(function(imagen) {
-
         const divSliderSection = document.createElement('div')
         const imgTag = document.createElement('img')
+
         imgTag.src = imagen.ruta
         imgTag.alt = imagen.texto
         divSliderSection.classList.add('slider-section')
         divSliderSection.appendChild(imgTag)
         divSlider.appendChild(divSliderSection)
     })
+
+    
+    const btnLeft = document.querySelector('.btn-left')
+    const btnRight = document.querySelector('.btn-right')
+    //console.log(btnLeft, btnRight)
+    const sliderSection = document.querySelectorAll('.slider-section')
+    //console.log(sliderSection)
+
+    let operacion = 0
+    let widthImg = 100 / sliderSection.length 
+    let counter = 0
+    console.log(widthImg)
+
+    function moverAlaDerecha() {
+        //debugger
+        if (counter >= sliderSection.length-1) {
+            //debugger
+            counter = 0
+            operacion = 0
+            console.log('mover a la derecha')
+            divSlider.style.transform = `translate(-${operacion}%)` 
+        } else {
+            //debugger
+            counter++
+            operacion = operacion + widthImg
+            divSlider.style.transform = `translate(-${operacion}%)`
+            divSlider.style.transition = 'transform ease .6s'
+        }
+    }
+
+    function moverAlaIzquierda() {
+        console.log('mover izquierda')
+        //debugger
+        counter--
+        if (counter < 0) {
+            //debugger
+            counter = sliderSection.length-1
+            operacion = widthImg * (sliderSection.length-1)
+            divSlider.style.transform = `translate(-${operacion}%)` 
+        } else {
+            //debugger
+            operacion = operacion - widthImg
+            divSlider.style.transform = `translate(-${operacion}%)`
+            divSlider.style.transition = 'transform ease .6s'
+        }
+    }
+
+    setInterval(function() {
+        moverAlaDerecha()
+    }, 3000)
+
+    
+    btnLeft.addEventListener('click', moverAlaIzquierda)
+    btnRight.addEventListener('click', moverAlaDerecha)
 
 })
 
